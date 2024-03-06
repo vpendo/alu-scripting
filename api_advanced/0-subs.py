d1118429668baf146a19a04d0ce16fc423a85766
v.pendo <v.pendo@alustudent.com>
2024-03-06 12:34:44 +0000

    api advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for the given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/" % subreddit + "/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
@@ -14,3 +14,4 @@ def number_of_subscribers(subreddit):
         if data:
             return data.get('subscribers', 0)
     return 0
+
