90312112be91126e9efddb33622d91f73077ef21
v.pendo <v.pendo@alustudent.com>
2024-03-06 14:40:15 +0000

    api advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Script that fetches the number of subscribers for a given subreddit."""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for the given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
