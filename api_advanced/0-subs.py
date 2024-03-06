154b1c74c17b721d5a8653fb4bbbcbe950cfba98
vpendo <v.pendo@alustudent.com>
2024-03-06 22:28:43 +0200

    API advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,10 +2,20 @@
 """Script that fetch 10 hot post for a given subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for the given subreddit."""
-    url = "https://www.reddit.com/r/{subreddit}/about.json"
+    """
+    Return the number of subscribers for the given subreddit.
+
+    If the subreddit is not found, returns 0 and prints an informative message.
+
+    Args:
+        subreddit (str): Name of the subreddit to query.
+
+    Returns:
+        int: Number of subscribers, or 0 if an error occurs.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
@@ -13,4 +23,9 @@ def number_of_subscribers(subreddit):
         data = response.json().get('data')
         if data:
             return data.get('subscribers', 0)
-    return 0
+        else:
+            print(f"Subreddit '{subreddit}' not found.")
+            return 0
+    else:
+        print(f"Error: Unable to retrieve data for subreddit '{subreddit}'.")
+        return 0
