f55c1b1c814e08e53a610f1b852b3ecbc5eea363
vpendo <v.pendo@alustudent.com>
2024-03-07 11:30:52 +0200

    API advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for the given subreddit."""
-    url = f"https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
